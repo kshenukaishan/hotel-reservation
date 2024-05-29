dc57ec19859de7d76f84fa25abd46f846f8b254c
kshenukaishan <kshenukaishan@outlook.com>
2024-05-29 19:54:36 +0530

    extract two modules together

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
+import { ReservationModule } from '../reservation/reservation.module';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [CommonModule, RouterModule],
+  exports: [HomeComponent],
 })
 export class HomeModule {}
